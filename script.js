9fabfe39e6d51f57503d0ef9598d223bc32390fc
vcnghiem <vincentnghiem@berkeley.edu>
2025-11-07 22:52:10 -0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoidmluY2VudG5naGllbSIsImEiOiJjbWg5Y2s2c3IwODR1MmpvbTJzM3Q5eTZqIn0.mRB-MTymbA4NHuNsZw5n3Q';
-
-const map = new mapboxgl.Map({
-  container: 'map', 
-    style: 'mapbox://styles/vincentnghiem/cmhn0x9rv001x01sr7efrg5p9', 
-  center: [-122.25939733965606, 37.871244106045195], 
-  zoom: 14.75
-    });
